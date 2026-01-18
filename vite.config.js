e31ac7371fe467a1632deae5b3d954fbad7977f6
Radin Khansari <156950334+radinkhansari@users.noreply.github.com>
2026-01-18 16:22:49 -0500

    Fix base path and tidy up vite.config.js
    Updated base path for Vercel deployment and cleaned up imports.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwinfdcss from '@tailwindcss/vite'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/E-Portfolio",
+  plugins: [react(), tailwindcss()],
+  base: "/", // ✅ FIX for Vercel
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
+
+
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+//import tailwinfdcss from '@tailwindcss/vite'
+//import tailwindcss from '@tailwindcss/vite'
+//import path from 'path'
+
+// https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react() , tailwindcss()],
+//  base: process.env.VITE_BASE_PATH || "/E-Portfolio",
+//  resolve: {
+//    alias: {
+//      '@': path.resolve(__dirname, './src'),
+//    },
+//  },
+//})
+
+
+
+
+
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
